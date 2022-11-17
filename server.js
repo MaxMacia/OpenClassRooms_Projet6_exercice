51b5ae7c00ae6ed48df9e50928a126d1149692a3
Maxence Macia <maxence.macia@gmail.com>
2022-11-17 17:43:29 +0100

    mise en place de l'app express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
+const app = require("./app");
 
-const server = http.createServer((req, res) => {
-    res.end("Voilà la nouvelle réponse du serveur!!")
-});
+app.set("port", process.env.POR || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.POR || 3000);
