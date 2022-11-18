4763a2b46a4d3f3a99bbe17118995883e337012e
Maxence Macia <maxence.macia@gmail.com>
2022-11-18 14:23:46 +0100

    connexion de l'API à MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    "mongodb+srv://MaxMacia:211089Mn@cluster0.ib5vpwf.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connexion à MongoDB réussie!"))
+    .catch(() => console.log("Connexion à MongoDB à échoué!"));
 
 app.use(express.json());
 
